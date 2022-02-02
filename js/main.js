dbfbaaab84a09c2990ba095866bab53e2d8f7deb
Jercovie Stewart <stewartjercovie@gmail.com>
2022-02-01 20:59:59 -0800

    Updated file

--- js/main.js
+++ js/main.js
@@ -14,3 +14,47 @@ function myFunction() {
         x.className = "topnav";
     }
 }
+
+
+
+const spans = document.querySelectorAll('div#play span');
+
+console.log(spans);
+
+for(const mySpan of spans){
+  mySpan.addEventListener('click', function(ev){
+
+    //alert(mySpan.dataset.actor)
+
+
+    highlightActor(mySpan.dataset.actor);
+
+  });
+}
+
+function myAlert(){
+  alert("I'm clicked");
+}
+
+function highlightActor(actor) {
+for(const mySpan of spans){
+  if(actor == mySpan.dataset.actor){
+    mySpan.style.backgroundColor = "yellow";
+  }else{
+    mySpan.style.backgroundColor = "white";
+  }
+}
+}
+
+function highlight(el) {
+  
+  if(el.style.backgroundColor == "yellow"){
+    el.style.backgroundColor = "white";
+  }else{
+    el.style.backgroundColor = "yellow";
+  }
+
+}
+
+
+  //document.querySelector('span.hamlet').style.backgroundColor = "yellow";
